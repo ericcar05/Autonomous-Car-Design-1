3014541768a118a921dee697dde2570959e4341d
ericcar05 <159899402+ericcar05@users.noreply.github.com>
2024-03-11 09:43:41 +0900

    Add files via upload

---
+++ Autonomous_Car_Design1/20240311/code1/code1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  //설정
+  Serial.begin(9600);
+  //Serial.print("test");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //반복실행
+  Serial.print("test");
+}
