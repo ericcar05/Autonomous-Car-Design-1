3fd5220683bef8b2e28c7b0435562821d8e54201
ericcar05 <159899402+ericcar05@users.noreply.github.com>
2024-03-29 12:44:29 +0900

    Add files via upload

---
+++ 20240329/linesensor-2/linesensor-2.ino
@@ -0,0 +1,51 @@
+#define ledpin 13
+#define LINE_DETECT_WHITE 1
+int linesensor_data[5]={0,0,0,0,0};
+int linesensor_pin[5]={2,3,4,5,6};
+
+int read_digital_line_sensor(void)
+{
+  int i;
+  int sum=0;
+  for(i=0;i<5;i++)
+  {
+    if(LINE_DETECT_WHITE==0)
+    {
+      linesensor_data[i]=1-digitalRead(linesensor_pin[i]);
+    }
+    else
+    {
+      linesensor_data[i]=digitalRead(linesensor_pin[i]);
+    }
+    sum+=linesensor_data[i];
+  }
+  return sum;
+}
+
+
+void setup() {
+  int i;
+  // put your setup code here, to run once:
+  pinMode(ledpin,OUTPUT);
+  for(i=0;i<5;i++)
+  {
+    pinMode(linesensor_pin[i],INPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  int sum=0;
+  sum=read_digital_line_sensor();
+  
+  Serial.print("input data=");
+  for(i=0;i<5;i++)
+  {
+    Serial.print(linesensor_data[i]);
+    Serial.print(" ");
+  }
+  Serial.print(sum);
+  Serial.println(" ");
+}
